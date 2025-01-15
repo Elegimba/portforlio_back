72cd6ab3c5c9b69af95cc595539c26eb082381e2
YagoGV <yagogv@gmail.com>
2025-01-15 20:12:45 +0100

    ajustes

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,3 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL);
-
-// Para cuando suba la BD a online hay que substituir la línea 3 por la siguiente:
-// mongoose.connect(process.env.MONGO_URL);
