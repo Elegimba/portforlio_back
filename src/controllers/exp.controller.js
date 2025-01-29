d0b7d9814c9064f72f42dd1b98148f5e7e3bca70
YagoGV <yagogv@gmail.com>
2025-01-29 18:39:28 +0100

    ordenados ahora sí

--- src/controllers/exp.controller.js
+++ src/controllers/exp.controller.js
@@ -2,7 +2,7 @@ const Experience = require('../models/exp.model');
 
 const getAll = async (req, res, next) => {
     try {
-        const exp = await Experience.find().sort({ createdAt: -1 });
+        const exp = await Experience.find().sort({ startYear: 1 });
         res.json(exp);
     } catch (error) {
         next(error);
