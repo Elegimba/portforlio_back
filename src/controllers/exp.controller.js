c1370b63b8d3494d77a0edd66901bf2c539a2b05
YagoGV <yagogv@gmail.com>
2025-01-29 18:00:12 +0100

    form y exp ordenados

--- src/controllers/exp.controller.js
+++ src/controllers/exp.controller.js
@@ -2,7 +2,7 @@ const Experience = require('../models/exp.model');
 
 const getAll = async (req, res, next) => {
     try {
-        const exp = await Experience.find();
+        const exp = await Experience.find().sort({ createdAt: -1 });
         res.json(exp);
     } catch (error) {
         next(error);
--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -2,7 +2,7 @@ const Formation = require('../models/form.model');
 
 const getAll = async (req, res, next) => {
     try {
-        const forms = await Formation.find();
+        const forms = await Formation.find().sort({ createdAt: -1 });
         res.json(forms);
     } catch (error) {
         next(error);
