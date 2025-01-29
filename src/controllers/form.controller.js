782f2934a6dd0cc5c8c619b129d18ab33ff93a17
YagoGV <yagogv@gmail.com>
2025-01-29 19:00:30 +0100

    ordenados ahora sí de verdad

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -2,7 +2,7 @@ const Formation = require('../models/form.model');
 
 const getAll = async (req, res, next) => {
     try {
-        const forms = await Formation.find().sort({ startYear: 1 });
+        const forms = await Formation.find().sort({ startYear: 1, endYear: 1 });
         res.json(forms);
     } catch (error) {
         next(error);
